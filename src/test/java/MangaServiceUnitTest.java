b769df0020f1d4e3a1e0149b22b29cceae2d81e0
tharindu-perera <31583817+tharindu-perera@users.noreply.github.com>
2020-09-23 14:00:24 +0530

    Initial commit

---
+++ src/test/java/MangaServiceUnitTest.java
@@ -0,0 +1,15 @@
+ import org.junit.Test;
+ import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+public class MangaServiceUnitTest {
+
+    @Test
+    public void testGetMangasByTitle() throws IOException {
+        User user = new User("tharindu");
+        assertEquals(user.getName() ,"tharindu");
+
+    }
+
+}
