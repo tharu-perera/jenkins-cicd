027dcf7c210c480e86217f581191bb324c4566b1
tharindu <tharindu.perera@ge.com>
2020-10-12 08:58:11 +0530

    merged maser

--- src/main/java/com/User.java
+++ src/main/java/com/User.java
@@ -1,7 +1,7 @@
 package com;
 
 public class User {
-     private int count;   //dwedwedwed  dcwdwedwdw
+     private int count;   //dwedwedwed  dcwdwedwdwdc
 
     public int getCount() {
         return count;
